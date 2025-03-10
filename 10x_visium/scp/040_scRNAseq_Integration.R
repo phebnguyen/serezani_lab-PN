afcf30a0596454f88abbb3b31a873be90ac19b35
phebnguyen <phoebe.u.nguyen@vanderbilt.edu>
2025-03-10 17:01:45 -0500

    Re-converted integrated_skin to Seurat object

--- 10x_visium/scp/040_scRNAseq_Integration.R
+++ 10x_visium/scp/040_scRNAseq_Integration.R
@@ -22,10 +22,14 @@ if (dir.exists(outputdir) = FALSE){
 
 # Convert the scanpy .h5ad file to a single cell experiment
 sce <- readH5AD(paste0(datadir, "integratedskindata.h5ad"))
-sce <- logNormCounts(sce)
+saveRDS(sce, file = paste0(outputdir, "integrated_skin_reference-sce.rds"))
 
 # Convert to Seurat, specifying the correct assays
-integrated_skin <- as.Seurat(sce, counts="counts", data="logcounts")
+integrated_skin <- as.Seurat(sce, counts="counts", data="X")
+# Rename the assay "originalexp" to "RNA"
+integrated_skin <- RenameAssays(integrated_skin, originalexp = "RNA")
+
+
 # saveRDS(integrated_skin, file = paste0(outputdir, "integrated_skin_reference.rds"))
 #Before merging common cell types together
 p <- DimPlot(integrated_skin, reduction = "X_umap", group.by = "leiden", label = TRUE)
@@ -33,41 +37,57 @@ p
 
 #Merge common cell types/leidin clusters together
 table(integrated_skin$leiden)
-rename_mapping <- c("EPI-1" = "Epidermal",
-                    "EPI-2" = "Epidermal",
-                    "EPI-3" = "Epidermal",
-                    "EPI-4" = "Epidermal",
-                    "EPI-5" = "Epidermal",
 
-                    "FIB-1" = "Fibroblasts",
-                    "FIB-2" = "Fibroblasts",
-                    "FIB-3" = "Fibroblasts",
-                    "FIB-4" = "Fibroblasts",
+# Ensure leiden is a character vector before renaming
+integrated_skin$leiden <- as.character(integrated_skin$leiden)
 
-                    "IMM-1" = "Immune",
-                    "IMM-2" = "Immune",
-                    "IMM-3" = "Immune",
+# Define the renaming mapping
+rename_mapping <- list("EPI-1" = "Epidermal",
+                       "EPI-2" = "Epidermal",
+                       "EPI-3" = "Epidermal",
+                       "EPI-4" = "Epidermal",
+                       "EPI-5" = "Epidermal",
+                       "FIB-1" = "Fibroblasts",
+                       "FIB-2" = "Fibroblasts",
+                       "FIB-3" = "Fibroblasts",
+                       "FIB-4" = "Fibroblasts",
+                       "IMM-1" = "Immune",
+                       "IMM-2" = "Immune",
+                       "IMM-3" = "Immune",
+                       "PERI" = "Pericytes",
+                       "ENDO" = "Endothelial",
+                       "SMC" = "Smooth Muscle Cells",
+                       "LYM-E" = "Lymphatic Endothelial",
+                       "SCH" = "Schwann Cells",
+                       "MEL" = "Melanocytes",
+                       "LC" = "Langerhans Cells",
+                       "SKEL" = "Skeletal Muscle")
 
-                    "PERI" = "Pericytes",
-                    "ENDO" = "Endothelial",
-                    "SMC" = "Smooth Muscle Cells",
+# Apply renaming safely
+integrated_skin$leiden <- sapply(integrated_skin$leiden, function(x) {
+  if (x %in% names(rename_mapping)) {
+    rename_mapping[[x]]
+  } else {
+    x  # Keep original name if not in mapping
+  }
+})
 
-                    "LYM-E" = "Lymphatic Endothelial",
-                    "SCH" = "Schwann Cells",
-                    "MEL" = "Melanocytes",
-                    "LC" = "Langerhans Cells",
-                    "SKEL" = "Skeletal Muscle")
-
-integrated_skin <- RenameIdents(integrated_skin, rename_mapping)
+# Convert back to factor (if needed)
+integrated_skin$leiden <- factor(integrated_skin$leiden)
 
 # Verify the renaming
 table(integrated_skin$leiden)
 
 #After merging common cell types together
-p <- DimPlot(integrated_skin, reduction = "X_umap", label = TRUE, pt.size = 0.5) + NoLegend()
+p <- DimPlot(integrated_skin, reduction = "X_umap", group.by = "leiden", label = TRUE, pt.size = 0.5) + NoLegend()
 ggsave(paste0(outputdir, "integrated_skin-UMAP.png"), plot = p, height = 10, width = 10, units = "in", dpi = 300)
 
-saveRDS(integrated_skin, file = paste0(outputdir, "integrated_skin_reference-merged.rds"))
+saveRDS(integrated_skin, file = paste0(outputdir, "integrated_skin_reference-Seurat.rds"))
+
+# SCT Transform the integrated_skin reference
+# Not enough RAM, try another dataset
+options(future.globals.maxSize = 4 * 1024^3)  # Set to 4GB
+integrated_skin <- SCTransform(integrated_skin, verbose = FALSE)
 
 ##### Integration with scRNAseq data #####
 
@@ -83,7 +103,8 @@ ab6 <- readRDS(paste0(datadir, "AB6_DR.rds"))
 ab7 <- readRDS(paste0(datadir, "AB7_DR.rds"))
 ab8 <- readRDS(paste0(datadir, "AB8_DR.rds"))
 
-#Test with AB1
+
+
 anchors <- FindTransferAnchors(reference = integrated_skin, query = ab1, normalization.method = "SCT")
 predictions.assay <- TransferData(anchorset = anchors, refdata = Idents(integrated_skin),
                                   prediction.assay = "TRUE", weight_reduction = ab1[["pca"]], dims = 1:30)
